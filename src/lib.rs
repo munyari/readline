47d96cb3364bce416c9cb84eaec15d82b97bcf48
Nicholas <npmazzuca@gmail.com>
2015-02-19 22:16:04 -0800

    [BREAKING CHANGE]:
    * Update to 0.0.6
    * Update readline-sys to 0.0.2
    * Return an Option<String> instead of a Result
    * Stop any unwrapping

--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,28 @@
-#![feature(io, std_misc)]
+#![feature(core, std_misc, libc)]
 extern crate "readline-sys" as ffi;
-use std::io::Result;
+
+extern crate libc;
+use self::libc::c_char;
 
 use std::ffi::{CString, c_str_to_bytes};
-use std::str;
+use std::string::String;
 
-pub fn readline(prompt: &str) -> Result<String> {
-    use std::io::{Error, ErrorKind};
+pub fn readline(prompt: &str) -> Option<String> {
     unsafe {
         // It doesn't matter if there is an interior null
         // It just won't prompt all the way 
-        let line_ptr = 
+        let line_ptr: *const c_char = 
             ffi::readline(CString::from_slice(prompt.as_bytes()).as_ptr());
 
         if line_ptr.is_null() {
-            return Err(Error::new(ErrorKind::Other, "end of file", None));
+            return None;
         }
 
-        let line = str::from_utf8(c_str_to_bytes(&(line_ptr as *const i8))).unwrap().to_string();
-
-        Ok(line)
+        Some(String::from_utf8_lossy(c_str_to_bytes(&line_ptr)).into_owned())
     }
 }
 
 pub fn add_history(line: &str) {
     let l = CString::from_slice(line.as_bytes()).as_ptr();
-    unsafe { ffi::add_history(l as *const i8) };
+    unsafe { ffi::add_history(l as *const c_char) };
 }
